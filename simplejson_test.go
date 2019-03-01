7745f8100b2969bd1239251a8b44262ce0793f01
赵威杰 <1060656096@qq.com>
2019-03-01 23:23:49 +0800

    aaa: 增加Del方法单元测试

--- simplejson_test.go
+++ simplejson_test.go
@@ -168,3 +168,29 @@ func TestJson_MustSetKeys(t *testing.T) {
 	assert.Equal(t, v2, "c程序设计.set.object.array.object")
 	assert.Equal(t, err, nil)
 }
+
+// 测试删除json对象
+func TestJson_DelSimpleObject(t *testing.T) {
+	body := jsonStr["simpleObject"]
+	j, err := NewJson([]byte(body))
+	// 删除map键
+	err = j.Del("name")
+	log.Println(j.data)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, j.data, make(map[string]interface{}))
+
+}
+
+// 测试删除json数组索引
+func TestJson_DelSimpleArray(t *testing.T) {
+	body := jsonStr["simpleArray"]
+	j, err := NewJson([]byte(body))
+	// 删除数组第2个元素
+	err = j.Del(1)
+	log.Println(j.data)
+	assert.Equal(t, j.data, []interface{}{"test2.01"})
+	// 删除数据第一个元素
+	j.Del(0)
+	assert.Equal(t, j.data, []interface{}{})
+	assert.Equal(t, err, nil)
+}
